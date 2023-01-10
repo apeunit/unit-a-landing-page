91dc1956e9533ded0662043f128527ee373a646e
Ntwali-Josue <kntwalijos@gmail.com>
2023-01-10 11:09:37 +0200

    fix: Change title heading tag

--- components/ui/Title.jsx
+++ components/ui/Title.jsx
@@ -1,8 +1,8 @@
-import {H3} from '../ui/Headings'
+import {H2} from '../ui/Headings'
 const Title = ({content}) => {
     return (
         <div className='bg-center text-white  bg-hero-pattern bg-no-repeat max-w-max'>
-            <H3 text={content} styles='pb-11.75 '></H3>
+            <H2 text={content} styles='pb-11.75 '></H2>
         </div>
     );
 }

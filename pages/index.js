ef8d63eac175ec6b24e6dd4189dc8397676dbe83
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-10 11:47:51 +0200

    feat: add default and large button components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import {DefaultButton, LargeButton} from "../components/ui/Button";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-center font-bold text-3xl text-indigo-700">Unit A landing Page</h1>;
+      <DefaultButton text="Learn more" hasArrow={true} />
 
+      <div className="my-10"></div>
+
+      <LargeButton text="Learn more" hasArrow={true} />
     </>
   )
 }

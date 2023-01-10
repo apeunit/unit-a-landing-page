aeff983d089d9a84072fdaa3692da9d1f252ab7e
mhenriette <mhenriette00@gnmail.com>
2023-01-10 12:09:19 +0200

    feat: add font family with local font

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-center font-bold text-3xl text-indigo-700">Unit A landing Page</h1>;
-      <Title content='who we are'></Title>
+      <Title content='Who we are'></Title>
 
     </>
   )

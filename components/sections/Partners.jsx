7c256dd39f7df3edce7a8d78cec672321e9d7fee
mhenriette <mhenriette00@gnmail.com>
2023-01-11 12:48:15 +0200

    fix: add keys to the mapped data

--- components/sections/Partners.jsx
+++ components/sections/Partners.jsx
@@ -8,14 +8,14 @@ const Partners = () => {
            <p className='text-secondary-200 -mt-12  text-center'>We partner with the  leading brands</p> 
             <div className='md:flex flex-wrap justify-center items-center hidden lg:mx-60'>
             {
-                logo.map((el, index)=>{return <Image priority width={el.Width} height={el.Height} src={el.source} alt='logo' id={index} className='mx-3 my-2'/>
+                logo.map((el, index)=>{return <Image priority width={el.Width} height={el.Height} src={el.source} alt='logo' key={index} className='mx-3 my-2'/>
                 })
             }
             </div>
             <div className='flex flex-wrap md:hidden justify-center w-full '>
                 {
                     logo.map((el, index)=>{
-                       return <div className='relative w-18.75 h-8.25 mx-1.5 my-2'> <Image id={index} src={el.source} priority fill alt='logo'/></div>
+                       return <div className='relative w-18.75 h-8.25 mx-1.5 my-2'key={index} > <Image  src={el.source} priority fill alt='logo'/></div>
 
 
                     })

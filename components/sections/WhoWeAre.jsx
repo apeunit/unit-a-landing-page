be3f8244d9fd6d0ce7ca2a7e0095826f6815887c
Delyc <d.twizeyima@alustudent.com>
2023-01-11 14:26:48 +0200

    Refactor: altering buttons by adding a tag

--- components/sections/WhoWeAre.jsx
+++ components/sections/WhoWeAre.jsx
@@ -8,7 +8,7 @@ const WhoWeAre = () => {
 
         <H5 styles="-mt-11.75" text="Ape Unit was founded in 2010 in Berlin. Since 2017 Ape Unit's Unit 8 has been working in the field of blockchain technology. We are working together with web3 protocols such as Ethereum, Tezos, Celo, NEAR,  Algorand, and Cosmos, but also for large international clients such as VW and small African Startups like BitLipa. 
 With the new Unit A, we want to shift our focus to African developers and create joint European and African organizations to work in blockchain technology."/>
-        <DefaultButton style="w-64.5 gap-6 md:w-fit text-2xl" text="Contact us" hasArrow={true} />
+        <DefaultButton style="w-64.5 gap-6 md:w-fit text-2xl" text="Contact us" hasArrow={true} hasLink={true}/>
         </section>
      );
 }
--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -1,18 +1,27 @@
 import Arrow from "../svgs/Arrow";
 
-export const DefaultButton = ({text, hasArrow, style}) => {
+export const DefaultButton = ({text, hasArrow, style, hasLink}) => {
     return ( 
         <button className={`bg-white rounded-8xl py-3 px-12 text-black font-normal tracking-tightest text-2xl flex lg:justify-between items-center lg:gap-x-9.75 group hover:px-8 duration-500 ${style}`}>
+            {hasLink ? <a href = "mailto: u2467@apeunit.com">
             <span className="-mt-1">{text}</span>
+            </a> : 
+            <span className="-mt-1">{text}</span>
+            }
+            
             {hasArrow && <Arrow  style="group-hover:rotate-45" />}
         </button>
     );
 }
 
-export const LargeButton = ({text, hasArrow, style}) => {
+export const LargeButton = ({text, hasArrow, style, hasLink}) => {
     return ( 
         <button className={`bg-white rounded-8xl py-4 px-12 text-black font-normal tracking-tightest text-3xl flex justify-between items-center gap-x-9.75 group hover:px-8 duration-500 ${style}`}>
+             {hasLink ? <a href = "mailto: u2467@apeunit.com">
             <span className="-mt-2">{text}</span>
+            </a> : 
+            <span className="-mt-2">{text}</span>
+            }
             {hasArrow && <Arrow  style="h-4.1575 w-4.0725 group-hover:rotate-45" />}
         </button>
     );

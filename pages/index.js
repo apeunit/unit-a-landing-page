52ff5d86fac18b37c85beb234cb5d872bc154550
Ntwali-Josue <kntwalijos@gmail.com>
2023-01-11 17:27:12 +0200

    Fix: Fix image size on mobile devices

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <>
       <Wrapper>
-        <OurWork />
         <WhoWeAre />
+        <OurWork />
         <WhatWeDo />
         <OurTeam />
         <Partners />

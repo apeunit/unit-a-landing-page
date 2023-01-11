959a75546bed6ae768efe682bccc60971e3871c4
mhenriette <mhenriette00@gnmail.com>
2023-01-11 15:42:29 +0200

    refactor: remove unnecessary spaces

--- components/sections/Partners.jsx
+++ components/sections/Partners.jsx
@@ -3,16 +3,16 @@ import Image from 'next/image';
 import logo from '../../data/logos'
 const Partners = () => {
     return (
-        < div className='flex flex-col items-center  justify-center '>
+        < div className='flex flex-col items-center justify-center'>
             <Title content='Partners'/>
-           <p className='text-secondary-200 -mt-12  text-center'>We partner with the  leading brands</p> 
+           <p className='text-secondary-200 -mt-12 text-center'>We partner with the  leading brands</p> 
             <div className='md:flex flex-wrap justify-center items-center hidden lg:mx-60'>
             {
                 logo.map((el, index)=>{return <Image priority width={el.Width} height={el.Height} src={el.source} alt='logo' key={index} className='mx-3 my-2'/>
                 })
             }
             </div>
-            <div className='flex flex-wrap md:hidden justify-center w-full '>
+            <div className='flex flex-wrap md:hidden justify-center w-full'>
                 {
                     logo.map((el, index)=>{
                        return <div className='relative w-18.75 h-8.25 mx-1.5 my-2'key={index} > <Image  src={el.source} priority fill alt='logo'/></div>

5868236b0c00ff3179526ea1547904cb0ee86304
Ntwali-Josue <kntwalijos@gmail.com>
2023-01-11 11:56:30 +0200

    Fix: Add required space between company Address details

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -4,22 +4,22 @@ import anchors from "../../data/footerAnchor";
 
 export default function Footer() {
   return (
-      <footer className="flex mx-10 text-white flex-col md:flex-row  md:justify-between  mb-12 mt-17.25  md:mt-31.75 xl:mt-33.75">
-        <p>
-          Ape Unit GmbH <br />
-          Waldemarstraße 38,
-          <br /> 10999 Berlin
-        </p>
-        <div className="flex flex-col gap-y-3.25 md:gap-y-0 pt-12 md:pt-0 ">
-          {anchors.map((anchor, anchorkey) => (
-            <Anchor key={anchorkey} href={anchor.link} style="after:hidden">
-              {anchor.content}
-            </Anchor>
-          ))}
-        </div>
-        <Link href="https://apeunit.com/impressum.txt">
-          <p className="pt-10 md:pt-0">Impressum / Privacy</p>
-        </Link>
-      </footer>
+    <footer className="flex xl:mx-10 text-white flex-col md:flex-row  md:justify-between  mb-12 mt-17.25  md:mt-31.75 xl:mt-33.75">
+      <div className="space-y-2">
+        <p>Ape Unit GmbH </p>
+        <p> Waldemarstraße 38,</p>
+        <p> 10999 Berlin</p>
+      </div>
+      <div className="flex flex-col pt-12 md:pt-0 ">
+        {anchors.map((anchor, anchorkey) => (
+          <Anchor key={anchorkey} href={anchor.link} style="after:hidden">
+            {anchor.content}
+          </Anchor>
+        ))}
+      </div>
+      <Link href="https://apeunit.com/impressum.txt">
+        <p className="pt-10 md:pt-0">Impressum / Privacy</p>
+      </Link>
+    </footer>
   );
 }

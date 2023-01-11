30eec46bb5d1c2a61696f6775497585972d76d6a
mhenriette <mhenriette00@gnmail.com>
2023-01-11 12:35:12 +0200

    feat: add Partners Section

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import {DefaultButton, LargeButton} from "../components/ui/Button";
 import Title from "../components/ui/Title";
 import polygon from '../public/assets/images/polygon.webp'
 import Wrapper from "../components/wrapper/Wrapper";
+import Partners from "../components/sections/Partners";
 export default function Home() {
   return (
     <>
@@ -15,6 +16,7 @@ export default function Home() {
          title="Blockchain is the internet  for cooperation"
          description="We believe that blockchain technology could have the same revolutionary potential as the internet and will change how we cooperate." 
          imagePath={polygon} />
+         <Partners/>
         </Wrapper>
     </>
   )

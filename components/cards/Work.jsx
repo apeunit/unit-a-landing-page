f9514e53ad5f0a2a65e2b27764fcf0e9d980bfa2
Ntwali-Josue <kntwalijos@gmail.com>
2023-01-11 11:20:00 +0200

    fix: Fix our work section margins

--- components/cards/Work.jsx
+++ components/cards/Work.jsx
@@ -5,7 +5,7 @@ import { DefaultButton } from "../ui/Button";
 
 const Work = ({ text, title, imageSrc2 }) => {
   return (
-    <div className="bg-primary-800 gap-x-6 max-w-7xl mb-6 lg:mb-8 min-w-min lg:max-h-170 text-white rounded-xl lg:mx-20 lg:pl-10 pt-10 lg:pt-32.75 pb-10 lg:pb-26.5 flex lg:flex-row flex-col items-center">
+    <div className="bg-primary-800 gap-x-6 max-w-7xl mb-6 lg:mb-8 min-w-min lg:max-h-170 text-white rounded-xl lg:mr-20 lg:pl-10 pt-10 lg:pt-32.75 pb-10 lg:pb-26.5 flex lg:flex-row flex-col items-center">
       <div className="max-w-147.5 min-w-147.5 px-5 md:pl-10 ">
         <H3 text={title}></H3>
         <P text={text} styles="text-secondary-200 leading-11.25 mb-6"></P>

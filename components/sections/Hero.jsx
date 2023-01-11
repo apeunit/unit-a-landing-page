95913f236407bf3784515569fbfd8bfc3c530104
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-11 16:00:03 +0200

    feat: adjust the design of Hero section on desktop, tablet, and mobile

---
+++ components/sections/Hero.jsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import { H4 } from "../ui/Headings";
+import HeroBall from "../../public/assets/images/hero-ball.webp";
+import HeroPrism from "../../public/assets/images/hero-prism.webp";
+import Scroll from "../../public/assets/images/scroll.webp";
+import UnderlineTop from "../../public/assets/images/underline-top.webp";
+import UnderlineBottom from "../../public/assets/images/underline-bottom.webp";
+
+const Hero = () => {
+    return ( 
+        <section className="flex flex-col pt-9.25">
+            <H4 text="Unit A" styles="text-white mb-11.75" />
+            <Image src={HeroBall} className="self-end lg:mr-17.15 hidden lg:flex" />
+            <h3 className={`font-semibold lg:text-14.75xl text-3xl lg:leading-22.125 leading-14.25 text-white lg:ml-20.25 lg:mr-12.5 mt-12.5 lg:mt-4.15 mb-23 w-full lg:w-239 xl:w-295.25`}>
+                <span>A </span> 
+                <p className="inline-flex">
+                     <span className="relative">
+                        joint-force b
+                        <Image src={UnderlineTop} className="absolute bottom-0 left-0" />
+                     </span>
+                </p>
+                <span>etween African & European developers & organizations working in </span> 
+                <p className="inline-flex">
+                     <span className="relative">
+                        blockchain technology
+                        <Image src={UnderlineBottom} className="absolute bottom-0 left-0" />
+                     </span>
+                </p>
+            </h3>
+
+            <div className="flex justify-center lg:grid lg:grid-cols-2 ml-4.75">
+                <Image src={HeroPrism} className="hidden lg:block" />
+                <Link href="#">
+                    <Image src={Scroll} />
+                </Link>
+            </div>
+        </section>
+    );
+}
+ 
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import {DefaultButton, LargeButton} from "../components/ui/Button";
 import Title from "../components/ui/Title";
 import polygon from '../public/assets/images/polygon.webp'
 import Wrapper from "../components/wrapper/Wrapper";
+import Hero from "../components/sections/Hero";
 export default function Home() {
   return (
     <>
       <Wrapper >
-      <h1 className="text-center font-bold text-3xl text-indigo-700">Unit A landing Page</h1>;
+        <Hero />
+      <h1 className="text-3xl font-bold text-center text-indigo-700">Unit A landing Page</h1>;
       <DefaultButton text="Learn more" hasArrow={true} />
       <LargeButton text="Learn more" hasArrow={true} />
       <Title content='Who we are'></Title>

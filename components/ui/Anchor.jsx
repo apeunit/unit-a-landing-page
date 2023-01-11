e787e61bf54672c6d95b58b773f526c62010534c
Ntwali-Josue <kntwalijos@gmail.com>
2023-01-11 11:57:05 +0200

    Fix: Remove uncessary spaces

--- components/ui/Anchor.jsx
+++ components/ui/Anchor.jsx
@@ -4,7 +4,7 @@ export default function Anchor({ href, children, style }) {
   return (
     <Link
       href={href}
-      className="inline-flex gap-1  md:leading-relaxed leading-loose font-normal md:text-2xl text-xl tracking-normal md:tracking-wide group"
+      className="inline-flex gap-1 md:leading-relaxed leading-loose font-normal md:text-2xl text-xl tracking-normal md:tracking-wide group"
     >
       <span
         className={`relative after:absolute after:bottom-0.5 after:left-0.5 after:right-0 after:w-auto after:h-[1px] group-hover:after:bg-transparent after:bg-black inline-block ${style}`}

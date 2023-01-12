95057daba85fc009a06bfed8ba4c63581a928e8b
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-12 17:10:02 +0200

    refactor: remove arbitrary classes

--- components/sections/OurTeam.jsx
+++ components/sections/OurTeam.jsx
@@ -4,12 +4,12 @@ import Title from "../ui/Title";
 import { WhiteArrow } from "../svgs/Arrow";
 const OurTeam = () => {
     return (
-        <section className="py-10 md:px-0 xl:w-[87.38%] mx-auto w-full  px-[26px]">
+        <section className="py-10 md:px-0 xl:w-87.38 mx-auto w-full  px-6.5">
             <Title content="Join Our Team" />
             {team.map((role, index) => {
                 return (
-                    <div key={index} className={`group flex items-center justify-between  gap-[19.4px] border-b-2 border-primary-800 py-6 ${role.id === 1 ? "border-t-2" : ""}`}>
-                        <H5 text={role.title} styles="text-white w-[277px] md:w-auto" />
+                    <div key={index} className={`group flex items-center justify-between  gap-4.85 border-b-2 border-primary-800 py-6 ${role.id === 1 ? "border-t-2" : ""}`}>
+                        <H5 text={role.title} styles="text-white w-69.25 md:w-auto" />
                         <WhiteArrow style="group-hover:rotate-45" />
                     </div>
                 )
--- components/wrapper/Wrapper.jsx
+++ components/wrapper/Wrapper.jsx
@@ -1,6 +1,6 @@
 const Wrapper = ({children}) => {
     return ( 
-       <section className="xl:mx-auto xl:max-w-[1280px] w-[1280px] xl:px-0 lg:px-9.5 px-5 max-w-[390px] md:max-w-[932px] mx-auto">
+       <section className="xl:mx-auto xl:max-w-320 w-320 xl:px-0 lg:px-9.5 px-5 max-w-97.5 md:max-w-233 mx-auto">
         {children}
        </section>
      );

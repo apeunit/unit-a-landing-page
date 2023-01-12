a3415e9df53650080f286f316b842fa2057c61ed
mhenriette <mhenriette00@gnmail.com>
2023-01-12 15:05:01 +0200

    fix: add spaces between sections

--- components/sections/OurWork.jsx
+++ components/sections/OurWork.jsx
@@ -4,7 +4,7 @@ import Work from "../cards/Work";
 
 const OurWork = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mt-6 lg:mt-24 mb-7.5 lg:mb-8">
       <Title content="Our Work" styles={'ml-5 md:ml-20'}/>
       <div>
         {work.map((el, index) => (

e0fad12dfaa75c16e0b444ef35bf45feb9e66bfe
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-13 12:04:34 +0200

    feat: animate images in what we do section

--- components/cards/WhatWeDoCard.jsx
+++ components/cards/WhatWeDoCard.jsx
@@ -1,8 +1,26 @@
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 import Image from 'next/image'
 import { H4 } from '../ui/Headings';
-import { motion } from 'framer-motion';
 
 const WhatWeDo = ({index, title, description, image}) => {
+    const boxVariant = {
+        visible: { opacity: 1, scale: 1, x: 0, transition: {duration: 0.5} },
+        hidden: { opacity: 0, scale: 0, x: index % 2 == 0 ? 200 : -200 },
+    }
+    
+    const control = useAnimation()
+    const [ref, inView] = useInView()
+
+    useEffect(() => {
+        if (inView) {
+          control.start("visible");
+        } else {
+            control.start("hidden");
+        }
+    }, [control, inView]);
+    
     return (
         <div className='flex flex-col md:flex md:flex-row md:justify-between  gap-8 md:grid-cols-2 py-8 md:py-14.25 md:mt-12.5'>
             <div className={`md:w-3/4 lg:w-153.5 xl:w-141.25 space-y-4 order-2 ${index % 2 !== 0 ? 'md:order-2 ' : 'md:order-1'}`}>
@@ -10,10 +28,11 @@ const WhatWeDo = ({index, title, description, image}) => {
                 <p>{description}</p>
             </div>
             <motion.div 
-            initial={{ x: "-80%", opacity: 0 , w: "10px", y: "30%"}}
-            animate={{ x: "0%", opacity: 1, y: "0%", w: "100%" }}
-            transition={{ duration: 0.7, ease: "easeIn" }}
-            className={`flex w-1/2  items-center md:justify-center order-1`}>
+                ref={ref}
+                variants={boxVariant}
+                initial="hidden"
+                animate={control}
+                className={`flex w-1/2  items-center md:justify-center order-1`}>
               <Image className={`${index % 2 !== 0 ? 'md:order-1' : 'md:order-2'}`} src={image} alt="image" />
             </motion.div>
         </div>
--- components/sections/WhatWeDo.jsx
+++ components/sections/WhatWeDo.jsx
@@ -1,7 +1,6 @@
 import offers from "../../data/offers";
 import WhatWeDoCard from "../cards/WhatWeDoCard";
 import Title from "../ui/Title";
-import { motion } from "framer-motion";
 const WhatWeDo = () => {
     return ( 
         <section className="px-6.5 xl:w-87.38 mx-auto md:px-0">

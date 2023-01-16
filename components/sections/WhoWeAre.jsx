0bcac124490982455af2bfe1e258d0c0710bb3f2
Ntwali-Josue <kntwalijos@gmail.com>
2023-01-16 18:42:56 +0200

    refactor: rename our work card props

--- components/sections/WhoWeAre.jsx
+++ components/sections/WhoWeAre.jsx
@@ -7,7 +7,7 @@ const WhoWeAre = () => {
           <Title>Who we are</Title> 
           <Heading5 className="-mt-11.75">Ape Unit was founded in 2010 in Berlin. Since 2017 Ape Unit{"'"}s Unit 8 has been working in the field of blockchain technology. We are working together with web3 protocols such as Ethereum, Tezos, Celo, NEAR,  Algorand, and Cosmos, but also for large international clients such as VW and small African Startups like BitLipa.</Heading5>
           <Heading5>With the new Unit A, we want to shift our focus to African developers and create joint European and African organizations to work in blockchain technology.</Heading5>
-          <DefaultButton style="w-64.5 gap-6 md:w-fit text-2xl" text="Contact us" hasArrow={true} hasLink={true} directLink="mailto: u2467@apeunit.com"/>
+          <DefaultButton style="w-64.5 gap-6 md:w-fit text-2xl" text="Contact us" hasArrow={true} hasLink={true} redirectLink="mailto: u2467@apeunit.com"/>
         </section>
      );
 }

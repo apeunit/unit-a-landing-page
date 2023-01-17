df246b7ff823257c6d2e28508e79cf03ca5ca66d
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-17 12:20:24 +0200

    fix: remove top margin on large button

--- components/ui/buttons/LargeButton.jsx
+++ components/ui/buttons/LargeButton.jsx
@@ -9,9 +9,9 @@ const LargeButton = ({ text, hasArrow, styleSpan, style, hasLink, redirectLink }
                 className={`bg-white h-17.5 rounded-8 py-4 px-12 text-black font-normal tracking-0.6 text-3xl flex justify-between items-center gap-x-9.75 group hover:px-8 duration-500 ${style}`}
             >
                 {hasLink ? (
-                    <span className="-mt-2">{text}</span>
+                    <span>{text}</span>
                 ) : (
-                    <span className={`-mt-2 ${styleSpan}`}>{text}</span>
+                    <span className={`${styleSpan}`}>{text}</span>
                 )}
                 {hasArrow && <Arrow style="h-4.16 w-4.07 group-hover:rotate-45" />}
             </button>

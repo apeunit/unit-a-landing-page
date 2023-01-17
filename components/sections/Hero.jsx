6af62136f54d9a6570ed2abcbc8d2765c355a898
Delyc <d.twizeyima@alustudent.com>
2023-01-17 17:35:06 +0200

    Fix: Adding space to span

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
             <Heading4 className="text-white mb-11.75 font-semibold">Unit A</Heading4>
             <Image src={HeroBall} className="self-end lg:mr-15.15 hidden lg:flex" alt="Hero Ball" />
             <h3 className="font-semibold md:text-14.75 text-3xl lg:leading-22.13 leading-14.25 text-white  mt-12.5 lg:mt-4.15 mb-23 w-full lg:w-239 xl:w-295.25 md:w-239 xl:ml-19.75 mx-auto">
-                <span>A</span>
+                <span>A </span>
                 <p className="inline-flex">
                     <span className="relative">
                         joint-force b

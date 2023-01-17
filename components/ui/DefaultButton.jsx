4d538d4f737945bdaf969e40efebc623177cdf0c
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-17 10:49:51 +0200

    fix: add height to default and large buttons

--- components/ui/DefaultButton.jsx
+++ components/ui/DefaultButton.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const DefaultButton = ({ text, hasArrow, style, hasLink, redirectLink }) => {
     return (
         <button
-          className={`bg-white rounded-8 py-3 px-12 text-black font-normal tracking-0.6 text-2xl flex lg:justify-between items-center lg:gap-x-9.75 group hover:px-8 duration-500 ${style}`}
+          className={`bg-white h-13.5 rounded-8 py-3 px-12 text-black font-normal tracking-0.6 text-2xl flex lg:justify-between items-center lg:gap-x-9.75 group hover:px-8 duration-500 ${style}`}
         >
           {hasLink ? (
             <Link href={redirectLink} target='_blank'>
--- components/ui/LargeButton.jsx
+++ components/ui/LargeButton.jsx
@@ -5,7 +5,7 @@ const LargeButton = ({ text, hasArrow, styleSpan, style, hasLink, redirectLink }
     return ( 
         <Link href={redirectLink} tagert="_blank">
             <button
-                className={`bg-white rounded-8 py-4 px-12 text-black font-normal tracking-0.6 text-3xl flex justify-between items-center gap-x-9.75 group hover:px-8 duration-500 ${style}`}
+                className={`bg-white h-17.5 rounded-8 py-4 px-12 text-black font-normal tracking-0.6 text-3xl flex justify-between items-center gap-x-9.75 group hover:px-8 duration-500 ${style}`}
             >
                 {hasLink ? (
                     <span className="-mt-2">{text}</span>

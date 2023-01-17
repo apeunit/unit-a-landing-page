01bf78e72ae3a15b04affc21e96cda5296bf305c
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-17 10:16:45 +0200

    refactor: wrap button with link to allow click on the whole button

--- components/ui/LargeButton.jsx
+++ components/ui/LargeButton.jsx
@@ -1,21 +1,21 @@
 import { Arrow } from "../svgs/Arrow";
 import Link from "next/link";
 
-const LargeButton = ({ text, hasArrow, styleSpan, style, hasLink }) => {
+const LargeButton = ({ text, hasArrow, styleSpan, style, hasLink, redirectLink }) => {
     return ( 
-        <button
-            className={`bg-white rounded-8 py-4 px-12 text-black font-normal tracking-0.6 text-3xl flex justify-between items-center gap-x-9.75 group hover:px-8 duration-500 ${style}`}
-        >
-        {hasLink ? (
-            <Link href={redirectLink} tagert='_blank'>
-            <span className="-mt-2">{text}</span>
-            </Link>
-        ) : (
-            <span className={`-mt-2 ${styleSpan}`}>{text}</span>
-        )}
-        {hasArrow && <Arrow style="h-4.16 w-4.07 group-hover:rotate-45" />}
-        </button>
-     );
+        <Link href={redirectLink} tagert="_blank">
+            <button
+                className={`bg-white rounded-8 py-4 px-12 text-black font-normal tracking-0.6 text-3xl flex justify-between items-center gap-x-9.75 group hover:px-8 duration-500 ${style}`}
+            >
+                {hasLink ? (
+                    <span className="-mt-2">{text}</span>
+                ) : (
+                    <span className={`-mt-2 ${styleSpan}`}>{text}</span>
+                )}
+                {hasArrow && <Arrow style="h-4.16 w-4.07 group-hover:rotate-45" />}
+            </button>
+        </Link>
+    );
 }
  
 export default LargeButton;

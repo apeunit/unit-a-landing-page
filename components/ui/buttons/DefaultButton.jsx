83de5b49085ad61ff1f302ffc008c675d900aae7
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-18 12:12:25 +0200

    fix: center content in work card and remove padding on hover for default and large buttons

--- components/ui/buttons/DefaultButton.jsx
+++ components/ui/buttons/DefaultButton.jsx
@@ -5,7 +5,7 @@ const DefaultButton = ({ text, hasArrow, className, redirectLink }) => {
   return (
     <Link href={redirectLink} target="_blank">
       <button
-        className={`bg-white h-13.5 rounded-8 py-3 px-12 text-black font-normal tracking-0.6 text-2xl flex md:justify-between items-center gap-6 lg:gap-x-9.75 group hover:px-8 duration-500 ${className}`}
+        className={`bg-white h-13.5 rounded-8 py-3 px-12 text-black font-normal tracking-0.6 text-2xl flex md:justify-between items-center gap-6 lg:gap-x-9.75 group duration-500 ${className}`}
       >
         <span className="-mt-1">{text}</span>
         {hasArrow && <Arrow className="group-hover:rotate-45" />}

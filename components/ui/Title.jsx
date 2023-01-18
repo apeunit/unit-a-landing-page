18b811bc529cf5273b309483d2ff758ba91b61ac
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-18 12:40:42 +0200

    fix: change font size of section headings

--- components/ui/Title.jsx
+++ components/ui/Title.jsx
@@ -2,7 +2,7 @@ import { Heading2 } from '../ui/Headings'
 const Title = ({ children, className }) => {
     return (
         <div className={`bg-center text-white bg-hero-pattern bg-no-repeat max-w-max ${className}`}>
-            <Heading2 className='pb-11.75'>{children}</Heading2>
+            <Heading2 className='pb-11.75 text-11.75'>{children}</Heading2>
         </div>
     );
 }

18181768448da96e31f891dfc6225c2732437d13
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-19 11:47:36 +0200

    fix: center the arrow down in relation to the polygon

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
                 </p>
             </h3>
 
-            <div className="flex justify-center lg:grid lg:grid-cols-2 ml-4.75">
+            <div className="flex justify-center lg:grid lg:grid-cols-2 ml-4.75 items-center">
                 <Image src={HeroPrism} className="hidden lg:block" alt="Prism" />
                 <Link href="#">
                     <Image src={downArrow} alt='Scroll' />

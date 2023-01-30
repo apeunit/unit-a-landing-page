0421cd114d0d5732cabb61a5be86ae04338910a6
mhenriette <mhenriette00@gnmail.com>
2023-01-30 20:10:47 +0200

    fix: remove unnecessary spacing

--- components/wrappers/PageWrapper.jsx
+++ components/wrappers/PageWrapper.jsx
@@ -1,6 +1,6 @@
 const PageWrapper = ({ children }) => {
   return (
-    <section className="xl:max-w-320 w-320 xl:px-0 lg:px-9.5 px-5 max-w-97.5 md:max-w-233 mx-auto">
+    <section className="xl:max-w-320 w-320 xl:px-0 px-5 max-w-97.5 md:max-w-233 mx-auto">
       {children}
     </section>
   );

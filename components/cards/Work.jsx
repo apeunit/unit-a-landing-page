97d685d50a2ebcb35a3f1f2037113ab19434f26a
Ntwali-Josue <kntwalijos@gmail.com>
2023-02-02 20:24:44 +0200

    Fix: Project screenshots to be links

--- components/cards/Work.jsx
+++ components/cards/Work.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Heading3, Paragraph } from "../ui/Typography";
 import DefaultButton from "../ui/buttons/DefaultButton";
 
@@ -7,20 +8,32 @@ const Work = ({ text, title, imageSrc, redirectLink }) => {
     <div className="pt-6 pb-8 xl:pt-32.75 xl:pb-27.5 bg-primary-800 w-full xl:w-320 rounded-8 flex-col lg:flex items-center lg:flex-row md:justify-between px-4 xl:px-0 xl:pl-12.75">
       <div className="xl:w-3/5 xl:pl-5 xl:pr-8 md:w-150">
         <Heading3 className="text-white">{title}</Heading3>
-        <Paragraph className="text-secondary-200 leading-11.25 mb-6 w-full">{text}</Paragraph>
+        <Paragraph className="text-secondary-200 leading-11.25 mb-6 w-full">
+          {text}
+        </Paragraph>
         <div className="w-44.75">
-          <DefaultButton text="Visit" hasArrow={true} style='gap-x-3 w-full' redirectLink={redirectLink} />
+          <DefaultButton
+            text="Visit"
+            hasArrow={true}
+            style="gap-x-3 w-full"
+            redirectLink={redirectLink}
+          />
         </div>
       </div>
-      <div
-        className={`lg:block w-157.5 flex justify-end lg:h-102 mt-10
+      <Link href={redirectLink} target="_blank">
+        <div
+          className={`lg:block w-157.5 flex justify-end lg:h-102 mt-10
         ${title === "Unit 8" ? "bg-unit8" : "bg-lab3"} bg-no-repeat rounded`}
-      />
+        />
+      </Link>
+
       <div
         className="lg:hidden relative h-48.5 mt-10 w-auto
           rounded lg:mx-10 xl:mx-5 md:w-212.5 md:h-115.75"
       >
-        <Image src={imageSrc} alt="our work" fill className="rounded" />
+        <Link href={redirectLink}>
+          <Image src={imageSrc} alt="our work" fill className="rounded" />
+        </Link>
       </div>
     </div>
   );
--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -7,35 +7,47 @@ import underlineTop from "/public/assets/images/underline-top.webp";
 import underlineBottom from "/public/assets/images/underline-bottom.webp";
 
 const Hero = () => {
-    return (
-        <section className="flex flex-col pt-9.25 mb-10.75 lg:mb-39.25">
-            <Heading4 className="text-white mb-11.75 font-semibold">Unit A</Heading4>
-            <Image src={heroBall} className="self-end lg:mr-15.15 hidden lg:flex" alt="Hero Ball" />
-            <h3 className="font-semibold md:text-14.75 text-9.5 lg:leading-22.13 leading-14.25 text-white  mt-12.5 lg:mt-4.15 mb-25.49 w-full lg:w-239 xl:w-295.25 md:w-239 xl:ml-19.75 mx-auto">
-                <span>A </span>
-                <p className="inline-flex">
-                    <span className="relative">
-                        joint-force b
-                        <Image src={underlineTop} className="absolute bottom-0 left-0" alt="Underline Top" />
-                    </span>
-                </p>
-                <span>etween African & European developers & organizations working in </span>
-                <p className="inline-flex">
-                    <span className="relative">
-                        blockchain technology
-                        <Image src={underlineBottom} className="absolute bottom-0 left-0" alt="Underline Bottom" />
-                    </span>
-                </p>
-            </h3>
+  return (
+    <section className="flex flex-col pt-9.25 mb-10.75 lg:mb-39.25">
+      <Heading4 className="text-white mb-11.75 font-semibold">Unit A</Heading4>
+      <Image
+        src={heroBall}
+        className="self-end lg:mr-15.15 hidden lg:flex"
+        alt="Hero Ball"
+      />
+      <h3 className="font-semibold md:text-14.75 text-9.5 lg:leading-22.13 leading-14.25 text-white  mt-12.5 lg:mt-4.15 mb-25.49 w-full lg:w-239 xl:w-295.25 md:w-239 xl:ml-19.75 mx-auto">
+        <span>A </span>
+        <p className="inline-flex">
+          <span className="relative">
+            joint-force b
+            <Image
+              src={underlineTop}
+              className="absolute bottom-0 left-0"
+              alt="Underline Top"
+            />
+          </span>
+        </p>
+        <span>
+          etween African & European developers & organizations working in{" "}
+        </span>
+        <p className="inline-flex">
+          <span className="relative">
+            blockchain technology
+            <Image
+              src={underlineBottom}
+              className="absolute bottom-0 left-0"
+              alt="Underline Bottom"
+            />
+          </span>
+        </p>
+      </h3>
 
-            <p className="text-6xl text-red-500 font-bold">Hello, testing deployment</p>
-
-            <div className="flex justify-center lg:grid lg:grid-cols-2 ml-4.75 items-center">
-                <Image src={heroPrism} className="hidden lg:block" alt="Prism" />
-                <Image src={downArrow} alt='Scroll' />
-            </div>
-        </section>
-    );
-}
+      <div className="flex justify-center lg:grid lg:grid-cols-2 ml-4.75 items-center">
+        <Image src={heroPrism} className="hidden lg:block" alt="Prism" />
+        <Image src={downArrow} alt="Scroll" />
+      </div>
+    </section>
+  );
+};
 
 export default Hero;

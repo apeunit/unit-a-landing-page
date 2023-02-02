dc0aac29ed1528d9dc33ccd8dfadd69e2b54cc52
mhenriette <mhenriette00@gnmail.com>
2023-02-02 12:39:59 +0200

    fix: change the typography referring to new version

--- components/sections/ContactUs.jsx
+++ components/sections/ContactUs.jsx
@@ -1,11 +1,11 @@
 import LargeButton from "../ui/buttons/LargeButton";
-import { Heading3 } from "../ui/Typography";
+import { Heading1 } from "../ui/Typography";
 
 const ContactUs = () => {
     return (
         <section className="bg-primary-800 mx-0.5 rounded-8 gap-16 xl:px-0 px-5 py-5.5 md:py-16 flex flex-col items-center">
             <div className="xl:w-3.73/5">
-                <Heading3 className="text-center text-white text-11.75">Ready to talk about your project?</Heading3>
+                <Heading1 className="text-center text-white text-11.75">Ready to talk about your project?</Heading1>
                 <div>
                     <p className="text-center text-white/90">Are you looking for a consultancy partnership to address your blockchain challenges? We&apos;d love to hear from you.  Let&apos;s build something</p>
                     <p className="text-center text-white/90">amazing together.</p>
--- components/sections/OurWork.jsx
+++ components/sections/OurWork.jsx
@@ -1,12 +1,12 @@
-import Title from "../ui/Title";
 import projects from "/data/projects";
 import Work from "../cards/Work";
+import { Heading2 } from "../ui/Typography";
 
 const OurWork = () => {
   return (
     <div className="flex flex-col mt-6 lg:mt-36 mb-7.5 lg:mb-16">
       <div>
-        <Title className="lg:ml-24 ml-0">Our Work</Title>
+        <Heading2 className="lg:ml-24 ml-0">Our Work</Heading2>
       </div>
       <div className="flex flex-col w-full gap-8 xl:mt-17">
         {projects.map((project, index) => (

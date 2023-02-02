681970fa79cf4086b84a1de1cb9e742251681505
mhenriette <mhenriette00@gnmail.com>
2023-02-02 14:56:00 +0200

    fix: change directory of favicon

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,10 @@ import WhatWeDo from "../components/sections/WhatWeDo";
 import ContactUs from "../components/sections/ContactUs";
 import Footer from "../components/layout/Footer";
 import Hero from "../components/sections/Hero";
-import Link from "next/link";
-
 const Home = () => {
   return (
     <>
       <Head>
-      <Link rel="icon" type="image/png" href="/assets/images/favicon.ico"/>
         <title>Unit A</title>
       </Head>
       <main>

8ff76c68fa80f4fe4c7796e1fe2f2e2efa2b458e
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-02 19:39:28 +0200

    change what we do section to contain Alain

--- components/sections/WhatWeDo.jsx
+++ components/sections/WhatWeDo.jsx
@@ -4,7 +4,7 @@ import Title from "../ui/Title";
 const WhatWeDo = () => {
     return (
         <section className="px-6.5 xl:w-4.37/5 mx-auto md:px-0">
-            <Title>What we do </Title>
+            <Title>What we do Alain</Title>
             {
                 offers.map((offer, index) => <WhatWeDoCard {...offer} key={index} index={index} />)
             }

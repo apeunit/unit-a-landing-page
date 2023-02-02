2a98b360b4fa597478c717104ec8118064d591e7
mhenriette <mhenriette00@gnmail.com>
2023-02-02 15:47:55 +0200

    fix: made the requested changes on typography

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Heading2 } from "../ui/Typography";
+import { Heading1 } from "../ui/Typography";
 import heroBall from "/public/assets/images/hero-ball.webp";
 import heroPrism from "/public/assets/images/hero-prism.webp";
 import downArrow from "/public/assets/images/downArrow.webp";
@@ -9,7 +9,7 @@ import underlineBottom from "/public/assets/images/underline-bottom.webp";
 const Hero = () => {
     return (
         <section className="flex flex-col pt-9.25 mb-10.75 lg:mb-39.25">
-            <Heading2 className="text-white mb-11.75 font-semibold">Unit A</Heading2>
+            <Heading1 className="text-white mb-11.75 font-semibold">Unit A</Heading1>
             <Image src={heroBall} className="self-end lg:mr-15.15 hidden lg:flex" alt="Hero Ball" />
             <h3 className="font-semibold md:text-14.75 text-9.5 lg:leading-22.13 leading-14.25 text-white  mt-12.5 lg:mt-4.15 mb-25.49 w-full lg:w-239 xl:w-295.25 md:w-239 xl:ml-19.75 mx-auto">
                 <span>A </span>

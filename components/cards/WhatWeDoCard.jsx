f637be211039590b0212468c12a9e310c6396017
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-03 10:01:38 +0200

    feat: change animation of images and disable translate on mobile screen

--- components/cards/WhatWeDoCard.jsx
+++ components/cards/WhatWeDoCard.jsx
@@ -1,14 +1,26 @@
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Image from 'next/image'
 import { Heading4 } from '../ui/Typography';
 
 const WhatWeDoCard = ({ index, title, description, image }) => {
+    const [screenSize, setScreenSize] =  useState(typeof window !== "undefined"  && window.innerWidth);
     const boxVariants = {
-        visible: { opacity: 1, scale: 1, x: 0, transition: { duration: 0.5 } },
-        hidden: { opacity: 0, scale: 0, x: index % 2 == 0 ? 100 : -100 },
-    }
+        visible: { opacity: 1, x: 0, transition: { duration: 0.9 } },
+        hidden: { opacity: 0, x: screenSize > 640 ? (index % 2 == 0 ? 150 : -150) : 0 },
+    };
+    
+    useEffect(() =>{
+        const setter = () => setScreenSize(window.innerWidth);
+        (() =>{
+            if(window){
+                setter()
+                window.addEventListener('resize', setter)
+            }
+        })();
+        return () => window?.removeEventListener('resize', setter)
+    }, []);
 
     const control = useAnimation()
     const [ref, inView] = useInView()
@@ -18,7 +30,7 @@ const WhatWeDoCard = ({ index, title, description, image }) => {
     }, [control, inView]);
 
     return (
-        <div className='flex flex-col md:flex md:flex-row md:justify-between gap-8 md:grid-cols-2 py-8 md:py-14.25 md:mt-12.5'>
+        <div className='flex flex-col md:flex md:flex-row md:justify-between gap-8 md:grid-cols-2 py-8 md:py-14.25 md:mt-12.5 overflow-x-hidden'>
             <div className={`md:w-3/4 lg:w-153.5 xl:w-141.25 space-y-4 order-2 ${index % 2 !== 0 ? 'md:order-2' : 'md:order-1'}`}>
                 <Heading4 className="xl:w-141.25">{title}</Heading4>
                 <p>{description}</p>

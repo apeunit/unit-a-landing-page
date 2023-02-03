ccc86c0466dc2445f725db57555a9323807c17fa
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-03 18:19:05 +0200

    refact: remove duplicate class names

--- components/sections/ContactUs.jsx
+++ components/sections/ContactUs.jsx
@@ -5,8 +5,8 @@ const ContactUs = () => {
     return (
         <section className="bg-primary-800 mx-0.5 rounded-8 gap-8 md:gap-16 xl:px-0 px-5 py-5.5 md:py-16 flex flex-col items-center">
             <div className="xl:w-3.73/5">
-                <Heading2 className="text-center text-white text-11.75">Ready to talk about your project?</Heading2>
-                <Paragraph className="mt-3 text-center text-white leading-6.75">
+                <Heading2 className="text-center">Ready to talk about your project?</Heading2>
+                <Paragraph className="mt-3 text-center text-white">
                     Are you looking for a consultancy partnership to address your blockchain challenges?
                     <br className="hidden md:block" />We&apos;d love to hear from you. Let&apos;s build something amazing together.
                 </Paragraph>

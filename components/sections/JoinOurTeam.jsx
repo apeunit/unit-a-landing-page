b5702b7ff59c3ce4dae05bf6a5a692e6bd19acec
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-03 15:03:15 +0200

    feat: modify openings data and update Join Our Team listing

--- components/sections/JoinOurTeam.jsx
+++ components/sections/JoinOurTeam.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import openings from "/data/openings";
 import {Heading2, Heading3} from "../ui/Typography";
 import WhiteArrow from "../svgs/WhiteArrow";
@@ -8,10 +9,10 @@ const JoinOurTeam = () => {
             <Heading2 className='bg-repeat-x'>Join Our Team</Heading2>
             {openings.map((opening, index) => {
                 return (
-                    <div key={index} className={`group flex items-center justify-between gap-4.85 border-b-2 border-primary-800 py-6.37 ${opening.id === 1 ? "border-t-2" : ""}`}>
+                    <Link href={opening.link} target="_blank" key={index} className={`group flex items-center justify-between gap-4.85 border-b-2 border-primary-800 py-6.37 ${opening.id === 1 ? "border-t-2" : ""}`}>
                         <Heading3 className="text-white w-69.25 md:w-auto">{opening.title}</Heading3>
                         <WhiteArrow className="group-hover:rotate-45"/>
-                    </div>
+                    </Link>
                 )
             })}
         </section>

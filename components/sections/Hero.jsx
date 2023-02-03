abb2c96e6cdf835666620d23837388d169b5d058
Ntwali-Josue <kntwalijos@gmail.com>
2023-02-03 14:36:49 +0200

    Fix: Hero section

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Heading4 } from "../ui/Typography";
+import { Heading1 } from "../ui/Typography";
 import heroBall from "/public/assets/images/hero-ball.webp";
 import heroPrism from "/public/assets/images/hero-prism.webp";
 import downArrow from "/public/assets/images/downArrow.webp";
@@ -7,15 +7,15 @@ import underlineTop from "/public/assets/images/underline-top.webp";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col pt-9.25 mb-10.75 lg:mb-39.25">
-      <Heading4 className="text-white mb-11.75 font-semibold">Unit A</Heading4>
+    <section className="text-white flex flex-col pt-9.25 mb-10.75 lg:mb-39.25">
+      <Heading1 className="mb-11.75">Unit A</Heading1>
       <div className="self-end w-10.5 h-10.5 lg:w-auto lg:h-auto lg:mr-15.15 lg:flex">
         <Image src={heroBall} alt="Hero Ball" />
       </div>
-      <h3 className="font- md:text-14.75 text-3xl lg:text-6xl lg:leading-22.13 leading-14.25 text-white  mt-12.5 lg:mt-4.15 mb-25.49 w-full lg:w-239 xl:w-295.25 md:w-239 xl:ml-19.75 mx-auto">
+      <Heading1 className="lg:mt-4.15 mb-25.49 w-full lg:w-239 xl:w-295.25 md:w-239 xl:ml-19.75 mx-auto">
         <span>A </span>
         <p className="inline-flex">
-          <span className="relative">
+          <span className="relative pr-2">
             joint-force
             <Image
               src={underlineTop}
@@ -30,7 +30,7 @@ const Hero = () => {
         <p className="inline-flex">
           <span className="relative">blockchain technology</span>
         </p>
-      </h3>
+      </Heading1>
 
       <div className="flex lg:justify-center lg:grid lg:grid-cols-2 lg:ml-4.75 items-center">
         <div className="h-8 w-8 lg:h-auto lg:w-auto mb-8">

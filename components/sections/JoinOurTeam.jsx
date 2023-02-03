a5ceb7236a0034d5086dca5790e1213bdc24c2b9
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-03 18:42:47 +0200

    feat: wrap opening listing with link that opens to external website

--- components/sections/JoinOurTeam.jsx
+++ components/sections/JoinOurTeam.jsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
 import openings from "/data/openings";
 import { Heading5 } from "../ui/Typography";
 import Title from "../ui/Title";
 import WhiteArrow from "../svgs/WhiteArrow";
+
 
 const JoinOurTeam = () => {
     return (
@@ -9,10 +11,10 @@ const JoinOurTeam = () => {
             <Title className='bg-repeat-x'>Join Our Team</Title>
             {openings.map((opening, index) => {
                 return (
-                    <div key={index} className={`group flex items-center justify-between gap-4.85 border-b-2 border-primary-800 py-6.37 ${opening.id === 1 ? "border-t-2" : ""}`}>
+                    <Link href={opening.link} target="_blank" key={index} className={`group flex items-center justify-between gap-4.85 border-b-2 border-primary-800 py-6.37 ${opening.id === 1 ? "border-t-2" : ""}`}>
                         <Heading5 className="text-white w-69.25 md:w-auto">{opening.title}</Heading5>
                         <WhiteArrow className="group-hover:rotate-45"/>
-                    </div>
+                    </Link>
                 )
             })}
         </section>

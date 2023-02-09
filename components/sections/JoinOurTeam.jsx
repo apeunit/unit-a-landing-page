dffaa16eb3b1f433d7de0795e8b812570b69412d
Delyc <d.twizeyima@alustudent.com>
2023-02-09 15:08:59 +0200

    Fix: adding text white on heading3 component

--- components/sections/JoinOurTeam.jsx
+++ components/sections/JoinOurTeam.jsx
@@ -5,7 +5,7 @@ import WhiteArrow from "../svgs/WhiteArrow";
 const JoinOurTeam = () => {
     return (
         <section className="py-10 md:px-0 xl:w-4.37/5 mx-auto w-full px-6.5">
-            <Heading2 className='bg-repeat-x'>Join Our Team</Heading2>
+            <Heading2 className='bg-repeat-x mb-5'>Join Our Team</Heading2>
             {openings.map((opening, index) => {
                 return (
                     <div key={index} className={`group flex items-center justify-between gap-4.85 border-b-2 border-primary-800 py-6.37 ${opening.id === 1 ? "border-t-2" : ""}`}>

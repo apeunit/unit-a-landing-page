4d884eca35c2bf309d6b6aef2093c3b4d37add86
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-09 16:46:42 +0200

    feat: reduce section margin on desktop

--- components/sections/WhatWeDo.jsx
+++ components/sections/WhatWeDo.jsx
@@ -3,7 +3,7 @@ import WhatWeDoCard from "../cards/WhatWeDoCard";
 import { Heading2 } from "../ui/Typography";
 const WhatWeDo = () => {
     return (
-        <section className="px-6.5 xl:w-4.37/5 mx-auto md:px-0">
+        <section className="px-6.5 xl:w-3.7/4 mx-auto md:px-0">
             <Heading2>What we do </Heading2>
             {
                 offers.map((offer, index) => <WhatWeDoCard {...offer} key={index} index={index} />)

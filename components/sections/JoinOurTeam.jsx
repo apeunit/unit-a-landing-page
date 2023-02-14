b0c04b25e6864c2532ccf9ab8a533a23be2bb274
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-14 15:37:17 +0200

    feat: add link to a single opening

--- components/sections/JoinOurTeam.jsx
+++ components/sections/JoinOurTeam.jsx
@@ -1,17 +1,18 @@
 import openings from "/data/openings";
 import {Heading2, Heading3} from "../ui/Typography";
 import WhiteArrow from "../svgs/WhiteArrow";
+import Link from "next/link";
 
 const JoinOurTeam = () => {
     return (
         <section className="py-10 md:px-0 xl:w-3.7/4 mx-auto w-full">
-            <Heading2 className='bg-repeat-x mb-5'>Join Our Team</Heading2>
+            <Heading2 className='mb-5 bg-repeat-x'>Join Our Team</Heading2>
             {openings.map((opening, index) => {
                 return (
-                    <div key={index} className={`group flex items-center justify-between gap-4.85 border-b-2 border-primary-800 py-6.37 ${opening.id === 1 ? "border-t-2" : ""}`}>
+                    <Link href={opening.link} target="_blank" key={index} className={`group flex items-center justify-between gap-4.85 border-b-2 border-primary-800 py-6.37 ${opening.id === 1 ? "border-t-2" : ""}`}>
                         <Heading3 className="text-white w-69.25 md:w-auto">{opening.title}</Heading3>
                         <WhiteArrow className="group-hover:rotate-45"/>
-                    </div>
+                    </Link>
                 )
             })}
         </section>

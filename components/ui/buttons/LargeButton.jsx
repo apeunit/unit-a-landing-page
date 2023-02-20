1c8c395842a403c4de23bf52fb668c5d2661c0bb
mhenriette <mhenriette00@gnmail.com>
2023-02-20 13:30:45 +0200

    fix: adjust the width of schedule a call button and fix the spacing in contact us section

--- components/ui/buttons/LargeButton.jsx
+++ components/ui/buttons/LargeButton.jsx
@@ -5,7 +5,7 @@ import { Heading4 } from "../Typography";
 const LargeButton = ({ text, hasArrow, styleSpan, className, hasLink, redirectLink }) => {
     return (
         <Link href={redirectLink} target="_blank"
-            className={`bg-white w-75 md:w-95.5 mx-auto h-17.5 rounded-11.25 py-4 px-6.5 gap-x-4 md:px-12 text-black font-normal tracking-0.6 flex justify-between items-center group duration-500 ${className}`}
+            className={`bg-white w-58.75 md:w-95.5 mx-auto h-17.5 rounded-11.25 py-3 px-6.5 gap-x-7.5 md:px-12 text-black font-normal tracking-0.6 flex justify-between items-center group duration-500 ${className}`}
         >
             {hasLink ? (
                 <Heading4>{text}</Heading4>

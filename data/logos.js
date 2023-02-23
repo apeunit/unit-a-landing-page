8cd1bb22ace5b52dce0490f8b8d66574a54976f3
Ntwali-Josue <kntwalijos@gmail.com>
2023-02-23 12:12:53 +0200

    Fix: Add dynamic image alt

--- data/logos.js
+++ data/logos.js
@@ -4,63 +4,72 @@ const logos = [
         source: '/assets/images/CELO.svg',
         Width: '120',
         Height: '59',
-        link: 'https://celo.org/'
+        link: 'https://celo.org/',
+        alt: "Celo"
     },
     {
         id: 2,
         source: '/assets/images/utu.svg',
         Width: '121',
         Height: '41',
-        link: 'https://utu.io/'
+        link: 'https://utu.io/',
+        alt: "Utu"
     },
     {
         id: 3,
         source: '/assets/images/Tezos.svg',
         Width: '109',
         Height: '59',
-        link: 'https://tezos.com/'
+        link: 'https://tezos.com/',
+        alt: "Tezos"
     },
     {
         id: 4,
         source: '/assets/images/symplifi.svg',
         Width: '142',
         Height: '59',
-        link: 'https://www.symplifi.co/'
+        link: 'https://www.symplifi.co/',
+        alt: "Symplifi"
     },
     {
         id: 5,
         source: '/assets/images/gizs.svg',
         Width: '69',
         Height: '59',
-        link: 'https://www.giz.de/de/html/index.html'
+        link: 'https://www.giz.de/de/html/index.html',
+        alt: "Giz"
     },
     {
         id: 6,
         source: '/assets/images/lab3.svg',
         Width: '99',
         Height: '60',
-        link: 'https://www.figma.com/proto/RbdGsuKF8RyVnrP5TTxdnO/Lab3-Presentation?node-id=1515%3A232&starting-point-node-id=1515%3A232'
+        link: 'https://www.figma.com/proto/RbdGsuKF8RyVnrP5TTxdnO/Lab3-Presentation?node-id=1515%3A232&starting-point-node-id=1515%3A232',
+        alt: "Lab 3"
     },
     {
         id: 7,
         source: '/assets/images/atem.svg',
         Width: '102',
         Height: '60',
-        link: 'https://www.atem.earth/'
+        link: 'https://www.atem.earth/',
+        alt: "Atem"
     },
     {
         id: 8,
         source: '/assets/images/bitlip.svg',
         Width: '135',
         Height: '59',
-        link: 'https://www.bitlipa.com/'
+        link: 'https://www.bitlipa.com/',
+        alt: "Bitlip"
     },
     {
         id: 9,
         source: '/assets/images/unit8.svg',
         Width: '124',
         Height: '60',
-        link: 'https://u8-landing.netlify.app/'
+        link: 'https://u8-landing.netlify.app/',
+        alt: "Unit 8"
     }
 ];
 

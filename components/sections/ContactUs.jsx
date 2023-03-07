053041fc4259d1d9357cd60996ff021ce9bfe71c
alain-kubwayo <ahkubwayo@gmail.com>
2023-03-07 07:24:25 +0200

    refactor: use double quotation marks around type

--- components/sections/ContactUs.jsx
+++ components/sections/ContactUs.jsx
@@ -5,7 +5,7 @@ const ContactUs = () => {
     return (
         <section className="bg-primary-800 mx-0.5 rounded-8 gap-8 md:gap-16 xl:px-0 px-4 py-12 md:py-16 flex flex-col items-center">
             <div className="xl:w-3.73/5">
-                <Heading className="text-center" type='one'>Ready to talk about your project?</Heading>
+                <Heading className="text-center" type="one">Ready to talk about your project?</Heading>
                 <Paragraph className="mt-3 text-center text-white">
                     Are you looking for a consultancy partnership to address your blockchain challenges?
                     <br className="hidden md:block" />We&apos;d love to hear from you. Let&apos;s build something amazing together.
